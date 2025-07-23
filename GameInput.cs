698f50509ce8c99d2be24d546a4bb089a72245bf
MinsungKxm <minsungk101@gmail.com>
2025-07-23 14:45:13 +0900

    Add files via upload

--- GameInput.cs
+++ GameInput.cs
@@ -6,18 +6,27 @@ using UnityEngine;
 public class GameInput : MonoBehaviour
 {
     public event EventHandler OnInteractAction;
+    public event EventHandler OnInteractAlternateAction;
+
     private PlayerInputActions playerInputActions;
+
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
         
+        // How to listen to when the Interact action is performed
         playerInputActions.Player.Interact.performed += Interact_performed;
+        playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
     }
+    private void InteractAlternate_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnInteractAlternateAction?.Invoke(this, EventArgs.Empty);
+    }
+
     // Start is called before the first frame update
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        OnInteractAction?.Invoke(this, EventArgs.Empty);
+        OnInteractAction?.Invoke(this, EventArgs.Empty);    
     }
 
     public Vector2 GetMovementVectorNormalized()
